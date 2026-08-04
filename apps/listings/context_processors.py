9ccd9db93b3aafb7380cd0bf725e5d619824bea5
ahmetelci2014-rgb <ahmetelci2014@gmail.com>
2026-08-04 15:49:56 +0300

    Create context_processors.py

---
+++ apps/listings/context_processors.py
@@ -0,0 +1,12 @@
+from .models import Notification
+
+
+def notification_counts(request):
+    if not request.user.is_authenticated:
+        return {"unread_notification_count": 0}
+    return {
+        "unread_notification_count": Notification.objects.filter(
+            user=request.user,
+            is_read=False,
+        ).count()
+    }
