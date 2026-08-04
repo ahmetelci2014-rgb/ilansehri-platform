a354fbba4badb274bba65a76e2ad6f13c7054034
ahmetelci2014-rgb <ahmetelci2014@gmail.com>
2026-08-04 14:04:11 +0300

    Update urls.py

--- apps/listings/urls.py
+++ apps/listings/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import ListingCreateView, ListingDetailView, ListingListView
+
+from .views import ListingCreateView, ListingDetailView, ListingListView, ListingUpdateView
 
 app_name = "listings"
+
 urlpatterns = [
     path("", ListingListView.as_view(), name="list"),
     path("yeni/", ListingCreateView.as_view(), name="create"),
+    path("<slug:slug>/duzenle/", ListingUpdateView.as_view(), name="update"),
     path("<slug:slug>/", ListingDetailView.as_view(), name="detail"),
 ]
