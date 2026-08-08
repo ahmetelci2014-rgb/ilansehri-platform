f27afe1deeb06230896078d2e588405b64de8730
ahmetelci2014-rgb <ahmetelci2014@gmail.com>
2026-08-08 07:01:51 +0000

    İlan Şehri v1.25.1 mobil denetim sürüm eşleşmesi

--- static/js/v132-mobile-system.js
+++ static/js/v132-mobile-system.js
@@ -122,7 +122,7 @@
     });
 
     window.__ILANSEHRI_MOBILE_AUDIT__ = {
-      version: "v1.25.0",
+      version: "v1.25.1",
       viewport: { width: viewportWidth, height: window.innerHeight },
       path: window.location.pathname,
       overflows: findings.slice(0, 50),
